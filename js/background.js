8a11c82bff71efe6e252befad37e5558192c69ae
GHeeJeon <jhjeon_99@naver.com>
2021-10-05 23:22:54 +0900

    change time
    2 hours!

--- js/background.js
+++ js/background.js
@@ -3,14 +3,30 @@ function updateBackground() {
         hr = (new Date()).getHours(),
         body = document.body,
         bstyle = body.style;
-    if (hr < 6) {
+    if (hr < 2) {
         bstyle.backgroundColor = "#5B6476";
-    } else if (hr < 12) {
+    } else if (hr < 4) {
         bstyle.backgroundColor = "#ECD9BA";
+    } else if (hr < 6) {
+        bstyle.backgroundColor = "#CECCB4";
+    } else if (hr < 8) {
+        bstyle.backgroundColor = "#CECCB4";
+    } else if (hr < 10) {
+        bstyle.backgroundColor = "#CECCB4";
+    } else if (hr < 12) {
+        bstyle.backgroundColor = "#CECCB4";
+    } else if (hr < 14) {
+        bstyle.backgroundColor = "#CECCB4";
+    } else if (hr < 16) {
+        bstyle.backgroundColor = "#CECCB4";
     } else if (hr < 18) {
         bstyle.backgroundColor = "#CECCB4";
+    } else if (hr < 20) {
+        bstyle.backgroundColor = "#CECCB4";
+    } else if (hr < 22) {
+        bstyle.backgroundColor = "#CECCB4";
     }else {
-        bstyle.backgroundColor = "#8CA1AE";
+        bstyle.backgroundColor = "#586068";
     }
 }
 // #ECD9BA #CECCB4 #5B6476
