f71aa3e6aac9ecfc56fee390e0a25b46c42691ae
GHeeJeon <jhjeon_99@naver.com>
2021-10-06 11:55:16 +0900

    update background color

--- js/background.js
+++ js/background.js
@@ -4,29 +4,29 @@ function updateBackground() {
         body = document.body,
         bstyle = body.style;
     if (hr < 2) {
-        bstyle.backgroundColor = "#5B6476";
+        bstyle.backgroundColor = "#576068";
     } else if (hr < 4) {
-        bstyle.backgroundColor = "#ECD9BA";
+        bstyle.backgroundColor = "#5B6575";
     } else if (hr < 6) {
-        bstyle.backgroundColor = "#CECCB4";
+        bstyle.backgroundColor = "#5B627C";
     } else if (hr < 8) {
-        bstyle.backgroundColor = "#CECCB4";
+        bstyle.backgroundColor = "#CECCB3";
     } else if (hr < 10) {
-        bstyle.backgroundColor = "#CECCB4";
+        bstyle.backgroundColor = "#E8DDC9";
     } else if (hr < 12) {
-        bstyle.backgroundColor = "#CECCB4";
+        bstyle.backgroundColor = "#ECE5DB";
     } else if (hr < 14) {
-        bstyle.backgroundColor = "#CECCB4";
+        bstyle.backgroundColor = "#EBE9E0";
     } else if (hr < 16) {
-        bstyle.backgroundColor = "#CECCB4";
+        bstyle.backgroundColor = "#EFEDE2";
     } else if (hr < 18) {
-        bstyle.backgroundColor = "#CECCB4";
+        bstyle.backgroundColor = "#ECE0C8";
     } else if (hr < 20) {
-        bstyle.backgroundColor = "#CECCB4";
+        bstyle.backgroundColor = "#ECDAB9";
     } else if (hr < 22) {
-        bstyle.backgroundColor = "#CECCB4";
+        bstyle.backgroundColor = "#A1A7B1";
     }else {
-        bstyle.backgroundColor = "#586068";
+        bstyle.backgroundColor = "#98979B";
     }
 }
 // #ECD9BA #CECCB4 #5B6476
