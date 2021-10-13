b5c2cfd29084901fe4c0cb5dbf6deff835a9acb3
GHeeJeon <jhjeon_99@naver.com>
2021-10-14 03:03:09 +0900

    Update result.js
    what should I do?

--- js/result.js
+++ js/result.js
@@ -14,10 +14,11 @@ function calResult() {
         console.log("공동 1등은 몇 개? : " + maxIndices.length);
         return maxIndices;
     }
+
     var finalSelect = maxIndices(select);
     var result;
     if (finalSelect.length > 1) {  // bbcc bccb abbc aaaa aaaa aaaa
-        //fill here
+        makeBonusQuestion();
         result = makeBonusQnA();
         console.log("if-result 잘 들어왔나요? " + result);
         console.log("중복 있어요!");
@@ -26,29 +27,32 @@ function calResult() {
         result = select.indexOf(Math.max(...select));
         console.log("else-result 잘 들어왔나요? " + result);
         console.log("중복 없어요!");
+        console.log("select 배열 : " + select);
         alert("중복 없어요!");
     }
 
-    console.log("select 배열 : " + select);
     console.log("result : " + result);
-    console.log("result type : " + typeof(result));
     return result;
 
-    function makeBonusQnA(){
+
+    function makeBonusQuestion(){
         var q = document.querySelector('.qBox');
         q.innerHTML = bonusList[0].qBonus;        // 질문: element 안의 HTML이나 XML 가져오기 <-> innerText
+
+    }
+
+    function makeBonusQnA() {
         var selectedResult;
         for (let i in finalSelect) {        // 답변: 버튼 만들기
             var finalSelectIndex = finalSelect[i];
             console.log(i + " 번째 " + "makeBonusAnswer 불러오기 시작!");
             selectedResult = makeBonusAnswer(bonusList[0].aBonus[finalSelectIndex].answer, finalSelectIndex);
-            console.log(i + " 번째 " + "makeBonusAnswer 불러오기 끝!");
             console.log("selectedResult 갱신완료! " + selectedResult);
+            console.log(i + " 번째 " + "makeBonusAnswer 불러오기 끝!");
         }
-        return selectedResult;
     }
-    function makeBonusAnswer(answerText, idx){
-        console.log(idx + " 번째 " + "makeBonusAnswer 만들기 시작!");
+
+    function makeBonusAnswer(answerText, idx) {
         var a = document.querySelector('.answerBox');
         var answer = document.createElement('button');  // button 만들어 반환
         answer.classList.add('answerList');       // answerList 라는 이름의 클래스값 부여
@@ -60,23 +64,20 @@ function calResult() {
 
         a.appendChild(answer);              // answer 가 a에 소속될 수 있게
         answer.innerHTML = answerText;      // element 안의 HTML이나 XML 가져오기
-        console.log(idx + " 번째 " + "makeBonusAnswer 만들기 끝!");
 
-        answer.addEventListener("click", function(){
+        answer.addEventListener("click", function () {
             var children = document.querySelectorAll('.answerList');  // 버튼을 모두 선택할 수 있게, html 의 onClick
-            for(let i = 0; i < children.length; i++){
+            for (let i = 0; i < children.length; i++) {
                 children[i].disabled = true;            // 버튼을 하나라도 누르면 모두 안보이게
                 children[i].style.WebkitAnimation = "fadeOut 0.5s";
                 children[i].style.animation = "fadeOut 0.5s";
             }
             setTimeout(() => {
-                console.log(bonusList[0].aBonus[idx].type + " 가 최종 선택되었습니다!");
-                console.log("타입 : " + typeof(bonusList[0].aBonus[idx].type));
-                for(let i = 0; i < children.length; i++){
+                for (let i = 0; i < children.length; i++) {
                     children[i].style.display = 'none';   // 버튼을 하나라도 누르면 모두 비활성화
                 }
-            },450)
-            console.log("answer.addEventListener 리턴값 : " + bonusList[0].aBonus[idx].type);
+                console.log(bonusList[0].aBonus[idx].type + " 가 최종 선택되었습니다!");
+            }, 450)
         }, false);
         return bonusList[0].aBonus[idx].type;           // 이 친구를 어디서 캐치해서 써먹을 건데?
     }
@@ -91,13 +92,11 @@ function setResult() {
     const imgDiv = document.querySelector('#resultImg');
     var imgURL = 'img/image-' + point + '.jpeg';       // image-n.jpeg
     resultImg.src = imgURL;
-    resultImg.alt = point;
     resultImg.classList.add('img-fluid');     // image 에 반응성 적용
     imgDiv.appendChild(resultImg);          // resultImg 연결
 
     const resultDesc = document.querySelector('.resultDesc');
     resultDesc.innerHTML = infoList[point].desc;
-
     goResult();
 }
 
