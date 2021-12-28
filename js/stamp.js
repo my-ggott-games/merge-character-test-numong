0eb67f6d1ac1803513bc5835686795d48ccd8485
GHeeJeon <jhjeon_99@naver.com>
2021-12-29 00:58:26 +0900

    make stamp
    awww so sweeeeet

--- js/stamp.js
+++ js/stamp.js
@@ -1,9 +1,9 @@
 function stamp() {
     var stampImg = document.createElement("img");
-    stampImg.src = "img/cursor.png";
+    stampImg.src = luckyStamp();
     stampImg.alt = "흰멍이 도장";
-    var x = event.clientX;
-    var y = event.clientY;
+    var x = event.clientX + 5;
+    var y = event.clientY + 10;
     stampImg.style.position = "absolute";
     stampImg.style.left = x + "px";
     stampImg.style.top = y + "px";
@@ -15,6 +15,14 @@ function stamp() {
         stampImg.style.animation = "fadeOut 1s";
         setTimeout(() => {
             stampImg.remove();
-        }, 1000)
-    }, 250)
+        }, 450)
+    }, 300)
+}
+
+function luckyStamp(){
+    const rand_0_17 = Math.floor(Math.random() * 18);
+    if (rand_0_17 === 17){
+        return "img/cursor_heart.png";
+    }
+    else return "img/cursor.png";
 }
